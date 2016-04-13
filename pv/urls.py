ad1f96922a88090e589c88e27bd66365fcc63df9
chawki benchehida <chawki27000@hotmail.com>
2016-04-13 11:57:11 +0100

    add pv in formateur actor

--- pv/urls.py
+++ pv/urls.py
@@ -4,5 +4,8 @@ from pv import views
 
 urlpatterns = [
 
-    url(r'^apprenant/pdf/$', views.note_apprenant, name='admin_order_pdf'),
+    url(r'^apprenant/pdf/info/etudiant$', views.note_apprenant, name='etudiant_pdf'),
+    url(r'^apprenant/dash$', views.PVApprenantView.as_view(), name='dash_apprenant'),
+    url(r'^formateur/dash$', views.PVFormateurView.as_view(), name='dash_formateur'),
+    url(r'^formateur/dash/info/liste$', views.all_apprenant, name='all_apprenant'),
 ]
--- pv/views.py
+++ pv/views.py
@@ -1,6 +1,8 @@
 from django.shortcuts import get_object_or_404
 
 # Create your views here.
+from django.views.generic import TemplateView
+
 from gestion.models import Apprenant
 from django.conf import settings
 from django.http import HttpResponse
@@ -16,3 +18,21 @@ def note_apprenant(request):
     weasyprint.HTML(string=html).write_pdf(response,
                                            stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
     return response
+
+
+class PVApprenantView(TemplateView):
+    template_name = "pv/dash_pv_apprenant.html"
+
+
+def all_apprenant(request):
+    article = Apprenant.objects.all()
+    html = render_to_string('pv/all_apprenant.html', {'article': article})
+    response = HttpResponse(content_type='application/pdf')
+    response['Content-Disposition'] = 'filename=\"apprenant.pdf"'
+    weasyprint.HTML(string=html).write_pdf(response,
+                                           stylesheets=[weasyprint.CSS(settings.STATIC_ROOT + 'css/pdf.css')])
+    return response
+
+
+class PVFormateurView(TemplateView):
+    template_name = "pv/dash_pv_formateur.html"
